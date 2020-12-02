ec1eca6e30e140561e5784f8bf4fcb2381f2bc17
Nyveon <54124297+Nyveon@users.noreply.github.com>
2020-12-02 13:09:02 -0300

    Day 2 part 1

---
+++ cmake-build-debug/day2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// Nyveon
+// Advent of code 2020
+
+int main() {
+
+    // Input
+    string passwords[1000];
+    char characters[1000];
+    int min_char[1000];
+    int max_char[1000];
+
+    ifstream input_file ("../day2_input.txt");
+    char char_dump;
+    char line_char;
+    string line;
+
+    int i = 0;
+    while (input_file >> min_char[i]) {
+        input_file >> char_dump >> max_char[i] >> characters[i] >> char_dump >> passwords[i];
+        i++;
+    }
+
+    // -- Part 1 --
+    int valid_passwords = 0;
+    for (int i = 0; i < 1000; i++) {
+        //string password = passwords[i];
+        int char_count = count(passwords[i].begin(), passwords[i].end(), characters[i]);
+
+        if (char_count <= max_char[i] && char_count >= min_char[i]) {
+            valid_passwords++;
+        }
+    }
+    // Output
+    cout << "Part 1: " << valid_passwords << "\n";
+
+
+    // -- Part 2 --
+    
+
+
+
+    return 0;
+}
--- day1.cpp
+++ day1.cpp
@@ -9,10 +9,10 @@ using namespace std;
 int main() {
 
     // Input
-    ifstream input_file ("../day1_input.txt");
     int a[200];
     int line;
     int i = 0;
+    ifstream input_file ("../day1_input.txt");
     while (input_file >> line) {
         a[i] = line;
         i++;
@@ -24,7 +24,7 @@ int main() {
         for (int j = 0; j < 200; j++) {
             if (a[i] + a[j] == 2020) {
                 // Output
-                cout << a[i] * a[j] << "\n";
+                cout << "Part 1: " << a[i] * a[j] << "\n";
                 goto superbreak;
             }
         }
@@ -39,7 +39,7 @@ int main() {
             for (int k = 0; k < 200; k++) {
                 if (a[i] + a[j] + a[k] == 2020) {
                     // Output
-                    cout << a[i] * a[j] * a[k] << "\n";
+                    cout << "Part 2: " << a[i] * a[j] * a[k] << "\n";
                     return 0;
                 }
             }
