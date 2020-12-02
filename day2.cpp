4d17e9fcead37332909a4511a1985a96402490a7
Nyveon <54124297+Nyveon@users.noreply.github.com>
2020-12-02 13:26:12 -0300

    organized a bit

--- day2.cpp
+++ day2.cpp
@@ -9,17 +9,14 @@ using namespace std;
 
 int main() {
 
-    // Input
+    // Variables
     string passwords[1000];
     char characters[1000];
-    int min_char[1000];
-    int max_char[1000];
-
-    ifstream input_file ("../day2_input.txt");
+    int min_char[1000], max_char[1000];
     char char_dump;
-    char line_char;
-    string line;
 
+    // -- Input --
+    ifstream input_file ("../day2_input.txt");
     int i = 0;
     while (input_file >> min_char[i]) {
         input_file >> char_dump >> max_char[i] >> characters[i] >> char_dump >> passwords[i];
@@ -29,28 +26,17 @@ int main() {
     // -- Part 1 --
     int valid_passwords = 0;
     for (int i = 0; i < 1000; i++) {
-        //string password = passwords[i];
         int char_count = count(passwords[i].begin(), passwords[i].end(), characters[i]);
-
-        if (char_count <= max_char[i] && char_count >= min_char[i]) {
-            valid_passwords++;
-        }
+        valid_passwords += (char_count <= max_char[i] && char_count >= min_char[i]);
     }
-    // Output
     cout << "Part 1: " << valid_passwords << "\n";
-
 
     // -- Part 2 --
     valid_passwords = 0;
     for (int i = 0; i < 1000; i++) {
-        if ((passwords[i][max_char[i]-1] == characters[i]) ^ (passwords[i][min_char[i]-1] == characters[i])) {
-            valid_passwords++;
-        }
+        valid_passwords += ((passwords[i][max_char[i] - 1] == characters[i]) ^ (passwords[i][min_char[i] - 1] == characters[i]));
     }
-    // Output
     cout << "Part 2: " << valid_passwords << "\n";
-
-
 
     return 0;
 }
